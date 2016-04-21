c368a2e1ddf776d3db902f1e7f4f8db7532ef254
Trevor <trevor@gotberg.net>
2016-04-21 16:58:02 -0600

    Update Server

--- LastStand/LastStand/server.js
+++ LastStand/LastStand/server.js
@@ -1,4 +1,42 @@
 ﻿var http = require('http'),
+    path = require('path'),
+    fs = require('fs'),
+    mimeTypes = {
+        '.js' : 'text/javascript',
+        '.html' : 'text/html',
+        '.css' : 'text/css'
+    };
+
+function handleRequest(request, response) {
+    var lookup = (request.url === '/') ? '/Scaffold/index.html' : decodeURI(request.url),
+        file;
+    console.log('request: ' + request.url);
+    file = lookup.substring(1, lookup.length);
+    fs.exists(file, function (exists) {
+        if (exists) {
+            fs.readFile(file, function (err, data) {
+                if (err) {
+                    response.writeHead(500);
+                    response.end('Server Error!');
+                } else {
+                    var headers = { 'Content-type': mimeTypes[path.extname(lookup)] };
+                    response.writeHead(200, headers);
+                    response.end(data);
+                }
+            });
+        } else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+}
+
+http.createServer(handleRequest).listen(3000, function () {
+    console.log('Server is listening');
+});
+
+/* Depricated
+var http = require('http'),
     port = process.env.port || 1337,
     path = require('path'),
     fs = require('fs'),
@@ -34,20 +72,4 @@ function handleRequest(request, response){
 http.createServer(handleRequest).listen(port, function () {
     console.log('Server is listening');
 });
-
-//var net = require('net'),
-//    server;
-   
-//function handleConnection(connection){
-//    connection.on('data', function (data) {
-//        console.log('data: ' + data);
-//        connection.write('Hi Mom!');
-//        connection.end();
-//    });
-//}
-
-//server = net.createServer(handleConnection);
-
-//server.listen(3000, function () {
-//    console.log('Server is listening');
-//});
+*/
